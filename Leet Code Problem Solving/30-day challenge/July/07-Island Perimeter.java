a85011fae97eb52c45e4db8c66675c1a78a81e93
jyjy0808 <47561241+jyjy0808@users.noreply.github.com>
2020-07-08 21:55:54 +0900

    last updated: 200708

--- Leet Code Problem Solving/30-day challenge/July/07-Island Perimeter.java
+++ Leet Code Problem Solving/30-day challenge/July/07-Island Perimeter.java
@@ -1,5 +1,26 @@
 class Solution {
+    public void dfs(int i, int j){
+        
+    }
     public int islandPerimeter(int[][] grid) {
-        // Backtracking maybe?
+        int count = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                int cellPer = 0;
+                if(grid[i][j] == 1){
+                    cellPer = 4;
+                    if(i-1 >= 0 && grid[i-1][j] == 1)
+                        cellPer--;
+                    if(i+1 < grid.length && grid[i+1][j] == 1)
+                        cellPer--;
+                    if(j-1 >= 0 && grid[i][j-1] == 1)
+                        cellPer--;
+                    if(j+1 < grid[i].length && grid[i][j+1] == 1)
+                        cellPer--;
+                }
+                count+=cellPer;   
+            }
+        }
+        return count;
     }
 }
